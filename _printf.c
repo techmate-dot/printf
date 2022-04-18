0b88aba4ec527afd3e6ba4f854532133e9445a59
techmate-dot <snwankwo218@gmail.com>
2022-04-18 12:35:45 +0000

    created a _printf function

---
+++ _printf.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *_printf - prints to stdout
+ *@format: string
+ *Return: returns the number count of the string character
+ */
+
+int _printf(const char *format, ...)
+{
+va_list arg_list;
+unsigned int = 0;
+if (format == NULL)
+{
+	exit (1);
+}
+convert_t f_list[] = {
+	{"c", print_char},
+	{"s", print_string},
+	{"d", print_integer},
+	{"i", print_integer},
+	{"u", unsigned_int},
+	{"x", print_hex},
+	{"X", print_heX},
+	{"%", print_percent},
+	{NULL, NULL}
+}
+
+va_start(arg_list, format);
+i = int parser(format, f_list, arg_list);
+return (i);
+}
