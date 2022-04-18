bd342fcb6b0564bd5f8d29eadc28afe7f0a1f88b
Hizaram <promisevector@gmail.com>
2022-04-18 23:39:37 +0100

    Solving the undefined reference problem

--- _printf.c
+++ _printf.c
@@ -1,5 +1,9 @@
 #include "main.h"
-#include <stdarg.h>
+#include "f_specifiers.c"
+#include "str-char.c"
+#include "parser.c"
+
+
 /**
  *_printf - prints to stdout
  *@format: string
@@ -8,19 +12,21 @@
 
 int _printf(const char *format, ...)
 {
-va_list arg_list;
-int char_count = 0;
+	int printed_chars;
 
-conver_t f_list[] = {
-	{"c", print_char},
-	{"s", print_string},
-	{"%", print_percent},
-	{NULL, NULL}
-};
+	conver_t f_list[] = {
+        {"c", print_char},
+        {"s", print_string},
+        {"%", print_percent},
+        {NULL, NULL}
+	};
 
-if (format == NULL)
-	exit(1);
-va_start(arg_list, format);
-char_count = parser(format, f_list, arg_list);
-return (char_count);
+
+	va_list arg_list;
+
+	if (format == NULL)
+		exit(1);
+	va_start(arg_list, format);
+	printed_chars = parser(format, f_list, arg_list);
+	return (printed_chars);
 }
